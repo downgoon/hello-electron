b4d65471a656dfeae41b484e1493e297a40a915a
downgoon <downgoon@qq.com>
2017-06-20 10:04:36 +0800

    user defined menu

--- main.js
+++ main.js
@@ -6,4 +6,26 @@ electron.app.on('ready', function createWindow () {
       {width: 800, height: 600}
     ).loadURL('file://' + __dirname + "/index.html")
 
+    var menuTemplate = electron.Menu.buildFromTemplate( [
+      {
+        label: '入门' ,
+        submenu: [
+          {
+            label: 'About ...',
+            click: () => {
+              console.log('HelloWorld on electron');
+            }
+          }, 
+          {
+            label: '退出',
+            click: () => {
+              electron.app.quit();
+            } 
+          }
+        ]
+      }
+    
+    ]);
+    electron.Menu.setApplicationMenu(menuTemplate);
+
 } );
